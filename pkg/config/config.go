8761c8571b71f747ade189214f0283d2550b5564
A-Hilaly <hilalyamine@gmail.com>
2018-07-11 11:47:16 +0200

    Add config support

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,35 +1,54 @@
 package config
 
-var (
-	Loaded bool
-	Config *config
-)
-
-type config struct {
-	Host    string
-	Port    string
-	Static  string
-	Uploads string
-	EP      *demoEndpointsConfig
+type Meta struct {
+	Name        string `yaml:"name"`
+	Description string `yaml:"description"`
+	Version     string `yaml:"version"`
+	Prod        bool   `yaml:"prod"`
 }
 
-type demoEndpointsConfig struct {
-	Inception string
-	Other     string
+type Server struct {
+	Mode     string `yaml:"mode"`
+	Port     int    `yaml:"port"`
+	Protocol string `yaml:"protocol"`
+	SSLTLS   bool   `yaml:"ssltls"`
 }
 
-func SetDefaults() {
-	Config = &config{
-		Port:    "8888",
-		Host:    "localhost",
-		Static:  "./static",
-		Uploads: "./static/demo",
-		EP: &demoEndpointsConfig{
-			Inception: "localhost:9000",
-		},
-	}
+type Database struct {
+	Type    string `yaml:"type"`
+	Prod    string `yaml:"prod"`
+	Debug   string `yaml:"debug"`
+	Test    string `yaml:"test"`
+	SSLMode int8   `yaml:"sslmode"`
+	Creds   struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+	} `yaml:"creds"`
 }
 
-func URL() string {
-	return "http://" + Config.Host + ":" + Config.Port + "/"
+type Session struct {
+	Type   string `yaml:"type"`
+	Random bool   `yaml:"random"`
+	Secret string `yaml:"secret"`
+	Name   string `yaml:"name"`
+}
+
+type Crypto struct {
+	Algo string `yaml:"algo"`
+	Salt string `yaml:"salt"`
+}
+
+type PublicContent struct {
+	Static string `yaml:"static`
+}
+
+type StartupConfig struct {
+	Meta     Meta          `yaml:"meta"`
+	Server   Server        `yaml:"server"`
+	Database Database      `yaml:"database"`
+	Session  Session       `yaml:"session"`
+	Crypto   Crypto        `yaml:"crypto"`
+	Public   PublicContent `yaml:"public"`
 }
---
+++ pkg/config/config_load.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	configPath    = "launch.yaml"
+	configPathGen = "launch-gen.yaml"
+)
+
+func SetDefaultPaths(path string) {
+	configPath = path + ".yaml"
+	configPathGen = path + "-gen.yaml"
+}
+
+func LoadConfigDefault() (*StartupConfig, error) {
+	return LoadConfigFromPath(configPath)
+}
+
+func LoadConfigFromPath(path string) (*StartupConfig, error) {
+	var config StartupConfig
+	configFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
