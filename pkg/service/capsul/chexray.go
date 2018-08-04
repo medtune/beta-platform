2c0b21f20424703eea93ceeab163a702ac63da5e
A-Hilaly <hilalyamine@gmail.com>
2018-08-04 18:11:34 +0200

    Set services capsules

--- pkg/service/capsul/chexray.go
+++ pkg/service/capsul/chexray.go
@@ -1,3 +1,34 @@
 // +build !cicd
 
 package capsul
+
+import (
+	"fmt"
+
+	"github.com/medtune/beta-platform/pkg/jsonutil"
+	inception "github.com/medtune/capsules/capsules/inception/v1"
+)
+
+func RunChexrayInference(infData *jsonutil.RunImageInference) (interface{}, error) {
+	if infData.File == "" {
+		return nil, fmt.Errorf("File field is empty: Got struct %v", infData)
+	}
+
+	// Run inference on image path (chexray/images)
+	resp, err := inception.RunInferenceOnImagePath(infData.File)
+	if err != nil {
+		return nil, err
+	}
+
+	// construct responses
+	result := jsonutil.InferenceResult{}
+	result.Scores = resp.Outputs["scores"].FloatVal
+
+	var s []string
+	for _, e := range resp.Outputs["classes"].StringVal {
+		s = append(s, string(e))
+	}
+	result.Keys = s
+
+	return result, nil
+}
--- pkg/service/capsul/mura.go
+++ pkg/service/capsul/mura.go
@@ -2,8 +2,33 @@
 
 package capsul
 
-import "github.com/medtune/beta-platform/pkg/jsonutil"
+import (
+	"fmt"
+
+	"github.com/medtune/beta-platform/pkg/jsonutil"
+	inception "github.com/medtune/capsules/capsules/inception/v1"
+)
 
 func RunMuraInference(infData *jsonutil.RunImageInference) (interface{}, error) {
-	return nil, nil
+	if infData.File == "" {
+		return nil, fmt.Errorf("File field is empty: Got struct %v", infData)
+	}
+
+	// Run inference on image path (mura/images)
+	resp, err := inception.RunInferenceOnImagePath(infData.File)
+	if err != nil {
+		return nil, err
+	}
+
+	// construct responses
+	result := jsonutil.InferenceResult{}
+	result.Scores = resp.Outputs["scores"].FloatVal
+
+	var s []string
+	for _, e := range resp.Outputs["classes"].StringVal {
+		s = append(s, string(e))
+	}
+	result.Keys = s
+
+	return result, nil
 }
